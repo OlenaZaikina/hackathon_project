ddf637c7c86f9c828cca9b131ff22abd4629a147
OlenaZaikina <olenazaykina92@gmail.com>
2020-12-18 01:48:42 +0200

    added initial version of social login

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import LandingContainer from './components/landingPage/LandingContainer';
 import './App.scss';
 import SignUp from './components/login/FormSignUp';
 import SignIn from './components/login/FormSignIn';
-import Dashboard from './components/login/Dashboard';
 import MainContainer from './components/main/MainContainer';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Route exact path="/" component={LandingContainer} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/dashboard" component={MainContainer} />
-      </Router>
-
-    </div>
+        <div className="App">
+          <Router>
+            <Route exact path="/" component={LandingContainer} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/dashboard" component={MainContainer} />
+          </Router>
+        </div>
   );
 }
 
--- src/components/main/MainContainer.js
+++ src/components/main/MainContainer.js
@@ -1,11 +1,12 @@
 import Header from './Header';
+import React from 'react'
 
 function MainContainer() {
-
+    let user = JSON.parse(localStorage.getItem('user'));
     return (
         <>
             <Header />
-
+            <h1>Hello { user._profile.firstName }!</h1>
         </>
     );
 }
