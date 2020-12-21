e7df4dd7b046126dc1d7888dd91921eae40f5d1a
Diana Kohut <dianakogut.088@gmail.com>
2020-12-21 16:51:45 +0200

    refactoring code

--- src/components/badges-page/BadgesPage.js
+++ src/components/badges-page/BadgesPage.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import usersList from '../../utils/fakeUser.json';
 import { badgesData } from '../../utils/badgesData';
 import BadgeText from './BadgeText';
 import comingSoon from '../../assets/coming-soon.svg';
+import { getBadgesRequest } from '../../service/userRequests';
 import './style.scss'
 
 const BadgesPage = () => {
@@ -10,46 +10,22 @@ const BadgesPage = () => {
 
     const [badges, setBadges] = useState([]);
     const [isFetching, setIsFetching] = useState(true);
-    const token = 'Bearer ' + JSON.parse(localStorage.getItem('user')).token;
-
-    const requestConfig = {
-        proxyurl: 'https://cryptic-mesa-87242.herokuapp.com/',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'http://localhost:3000',
-            'Access-Control-Allow-Credentials': 'true',
-        },
-    }
-
 
     const getBadges = async () => {
         try {
-            console.log('isFetching before request done',isFetching)
             if (isFetching) {
-
-                console.log()
-                const response = await fetch(requestConfig.proxyurl + 'http://34.222.107.139:8080/goaltracker/api/user/badges', {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': 'http://localhost:3000',
-                        'Access-Control-Allow-Credentials': 'true',
-                        'Authorization': token
-                    }
-                });
-                console.log('response', response)
+                const response = await getBadgesRequest();
                 const data = await response.json();
-
-                console.log(data);
                 setBadges(data);
                 setIsFetching(false);
-                console.log('isFetching after request done',isFetching)
-
             }
         } catch (err) {
             console.log(err.message)
+            return err.message;
         }
     }
     getBadges();
+
     return (
         <div className="badges-page-container">
             {isFetching && <p> Loading...</p>}
--- src/service/requestConfig.js
+++ src/service/requestConfig.js
@@ -1,20 +1,23 @@
-// const token = 'Bearer ' + JSON.parse(localStorage.getItem('user')).token;
-// const proxyurl = "https://cors-anywhere.herokuapp.com/";
-// let headers = new Headers();
 
-// headers.append('Content-Type', 'application/json');
-// headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
-// headers.append('Access-Control-Allow-Credentials', 'true');
 
-const requestConfig = {
+// const requestConfig = {
+//     proxyurl: 'https://cryptic-mesa-87242.herokuapp.com/',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Access-Control-Allow-Origin': 'http://localhost:3000',
+//         'Access-Control-Allow-Credentials': 'true',
+//         'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+//     },
+//     token: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+// }
+const token = 'Bearer ' + JSON.parse(localStorage.getItem('user')).token;
+export const requestConfig = {
     proxyurl: 'https://cryptic-mesa-87242.herokuapp.com/',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': 'http://localhost:3000',
         'Access-Control-Allow-Credentials': 'true',
-        // 'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+        'Authorization': token
     },
-    // token: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+    token
 }
-
-export default requestConfig;
