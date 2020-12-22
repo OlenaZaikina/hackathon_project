8488f8ec467b8cbaffb79d5b1ac1dc80c228e2d6
Diana Kohut <dianakogut.088@gmail.com>
2020-12-22 20:18:10 +0200

    small refactoring

--- src/service/requestsConfig.js
+++ src/service/requestsConfig.js
@@ -1,31 +1,9 @@
-const requestsConfig = {
+export const requestsConfig = {
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': 'http://localhost:3000',
         'Access-Control-Allow-Credentials': 'true',
         'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
-    }
+    },
+    proxyurl: 'https://cryptic-mesa-87242.herokuapp.com/'
 }
-
-
-
-
-
-export const getUserGoals = async () => {
-    try {
-        const response = await fetch('https://cryptic-mesa-87242.herokuapp.com/' + 'http://34.222.107.139:8080/goaltracker/api/users/goals', {
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': 'http://localhost:3000',
-                'Access-Control-Allow-Credentials': 'true',
-                'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
-            }
-        }
-        )
-        const data = await response.json()
-        return data;
-    } catch (err) {
-        return err.message;
-    }
-}
-
