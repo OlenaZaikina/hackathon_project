e9414f1ec5033dcdbd746990255136b13a7a1d5d
Diana Kogut <61844898+DianaKogut@users.noreply.github.com>
2020-12-22 12:09:29 +0200

    Update GoalInformation.js

--- src/components/tracking-progress/GoalInformation.js
+++ src/components/tracking-progress/GoalInformation.js
@@ -2,21 +2,14 @@
 
 const GoalInformation = ({ goal }) => {
 
-
-    const goalId = () => {
-        console.log(goal.id)
-    }
-
-
     const updateGoalInfo = async () => {
         try {
             console.log(goal.daysPassed+1)
             const body = JSON.stringify({
-                id: goal.id,
                 daysPassed: goal.daysPassed + 1
             })
             console.log('goal id in request', goal.id)
-            const response = await fetch('https://cryptic-mesa-87242.herokuapp.com/' + `http://34.222.107.139:8080/goaltracker/api/users/goals`, {
+            const response = await fetch('https://cryptic-mesa-87242.herokuapp.com/' + `http://34.222.107.139:8080/goaltracker/api/users/goals/${goal.id}`, {
                 headers: {
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': 'http://localhost:3000',
@@ -27,17 +20,36 @@ const GoalInformation = ({ goal }) => {
                 body
             })
 
-
             console.log('response', response);
 
             const data = await response.json();
-            console.log('data',data)
+            console.log('data', data)
         } catch (err) {
             console.log(err.message)
             return err.message;
         }
     }
 
+    const deleteGoal = async () => {
+        try {
+            console.log('goal id in request', goal.id)
+            const response = await fetch('https://cryptic-mesa-87242.herokuapp.com/' + `http://34.222.107.139:8080/goaltracker/api/users/goals/${goal.id}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': 'http://localhost:3000',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+                },
+                method: "DELETE"
+            })
+
+            console.log('response', response);
+
+        } catch (err) {
+            console.log(err.message)
+            return err.message;
+        }
+    }
 
     return (
         <div className="tracking-goal-information">
@@ -47,7 +59,7 @@ const GoalInformation = ({ goal }) => {
             <p>{goal.id}</p>
             <p>Want to update your progress? </p>
             <button className="update-goal" onClick={() => updateGoalInfo()}>Upgrade progress</button>
-            <button className="delete-goal">Delete goal</button>
+            <button className="delete-goal" onClick={() => deleteGoal()}>Delete goal</button>
         </div>
     )
 }
